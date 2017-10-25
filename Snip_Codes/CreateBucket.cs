70782f6977c89e5d025035bd30d349fa281234e7
DeepakNautiyal987 <deepaknautiyal07@outlook.com>
2017-10-25 23:54:10 +0530

    Create S3 Bucket

---
+++ Snip_Codes/CreateBucket.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace NewBucket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string accessKey = "*** Enter Access Key Here ***";
+            string secretKey = "*** Enter Secret Key Here ***";
+            string NameOFTheBucket = "*** Name of the Bucket Here ***";
+            AmazonS3Client client = new AmazonS3Client(accessKey, secretKey, Amazon.RegionEndpoint.APSouth1);
+            PutBucketRequest request = new PutBucketRequest
+            {
+                BucketName = NameOFTheBucket,
+                UseClientRegion = true
+            };
+            client.PutBucket(request);
+            Console.WriteLine("Bucket Created");
+            Console.ReadLine();
+        }
+    }
+}
