e47b96b1ccb632c1f6813eb3195ada20cb553f97
DeepakNautiyal987 <deepaknautiyal07@outlook.com>
2017-11-05 23:39:39 +0530

    Cross Region Replication

---
+++ Snip_Codes/CrossRegionReplication.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Amazon.S3.Model;
+using Amazon.S3;
+
+namespace CrossRegionReplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string AccessKey = "";
+            string SecretKey = "";
+            string Source_Bucket = " *** Source Bucket Here *** ";
+            string Destination_Bucket = " *** Destination Bucket Here *** ";
+            string Source_ObjectKey = " *** Source Object Key *** ";
+            string Destination_ObjectKey = " *** Destination Object Key *** ";
+            AmazonS3Client client = new AmazonS3Client(AccessKey, SecretKey, Amazon.RegionEndpoint.APSouth1);
+            try
+            {
+                CopyObjectRequest Request = new CopyObjectRequest
+                {
+                    SourceBucket = Source_Bucket,
+                    DestinationBucket = Destination_Bucket,
+                    SourceKey = Source_ObjectKey,
+                    DestinationKey = Destination_ObjectKey
+                };
+                client.CopyObject(Request);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("ERROR MESSAGE" + e.Message);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
+
+
+//string Source_Bucket = " *** Source Bucket Here *** ";
+//string Destination_Bucket = " *** Destination Bucket Here *** ";
+//string Source_ObjectKey = " *** Source Object Key *** ";
+//string Destination_ObjectKey = " *** Destination Object Key *** ";
