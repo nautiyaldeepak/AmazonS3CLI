d1f740adc895f634e69556958f824316e7bec25b
Deepak Nautiyal <30626234+nautiyaldeepak@users.noreply.github.com>
2018-04-21 23:56:16 +0530

    UploadToS3

--- Snip_Codes/UploadToS3.cs
+++ Snip_Codes/UploadToS3.cs
@@ -10,6 +10,44 @@ namespace TransferDataToS3
     // adding regions in progress
     class Program
     {
+        public static string ClientRegion(string RegionOfClient)
+        {
+            switch(RegionOfClient)
+            {
+                case "ireland":
+                    return "eu-west-1";
+                case "mumbai":
+                    return "ap-south-1";
+                case "frankfurt":
+                    return "eu-central-1";
+                case "london":
+                    return "eu-west-2";
+                case "sydney":
+                    return "ap-southeast-2";
+                case "ohio":
+                    return "us-east-2";
+                case "virginia":
+                    return "us-east-1";
+                case "california":
+                    return "us-west-1";
+                case "oregon":
+                    return "us-west-2";
+                case "singapore":
+                    return "ap-southeast-1";
+                case "tokyo":
+                    return "ap-northeast-1";
+                case "canada":
+                    return "ca-central-1";
+                case "seoul":
+                    return "ap-northeast-2";
+                case "paris":
+                    return "eu-west-3";
+                case "sao paulo":
+                    return "sa-east-1";
+            }
+            return "";
+        }
+        
         public static int UploadToS3()
         {
             string AccessKey = "*** Enter Access Key ***";
