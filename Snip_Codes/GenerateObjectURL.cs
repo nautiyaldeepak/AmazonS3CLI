79605d7248ed996dae4981d5cdc540b99d54fa18
nautiyaldeepak <deepaknautiyal07@outlook.com>
2018-05-09 19:33:04 +0530

    Generate Object URL

--- Snip_Codes/GenerateObjectURL.cs
+++ Snip_Codes/GenerateObjectURL.cs
@@ -8,6 +8,49 @@ namespace GenerateObjectURL
 {
     class Program
     {
+        public static string ClientRegion(string RegionOfClient)
+        {
+            switch (RegionOfClient)
+            {
+                case "ireland":
+                    return "eu-west-1";
+                case "mumbai":
+                    return "ap-south-1";
+                case "frankfurt":
+                    return "eu-central-1";
+                case "london":
+                    return "eu-west-2";
+                case "sydney":
+                    return "ap-southeast-2";
+                case "ohio":
+                    return "us-east-2";
+                case "virginia":
+                    return "us-east-1";
+                case "california":
+                    return "us-west-1";
+                case "oregon":
+                    return "us-west-2";
+                case "singapore":
+                    return "ap-southeast-1";
+                case "tokyo":
+                    return "ap-northeast-1";
+                case "canada":
+                    return "ca-central-1";
+                case "seoul":
+                    return "ap-northeast-2";
+                case "paris":
+                    return "eu-west-3";
+                case "sao paulo":
+                    return "sa-east-1";
+                case "osaka-local":
+                    return "ap-northeast-3";
+                case "beijing":
+                    return "cn-north-1";
+                case "ningxia":
+                    return "cn-northwest-1";
+            }
+            return "";
+        }
         static void Main(string[] args)
         {
             string AccessKey = " *** Enter Access Key Here *** ";
@@ -17,12 +60,9 @@ namespace GenerateObjectURL
             string RegionOfTheBucket = " *** Enter The Region Of The Bucket (Eg: mumbai) ***";
             RegionOfTheBucket = RegionOfTheBucket.ToLower();
             
-            // The region of the client is Mumbai -> APSouth1
-            //  Amazon Client provides access to S3
-            
             try
             {
-                AmazonS3Client client = new AmazonS3Client(AccessKey, SecretKey, Amazon.RegionEndpoint.APSouth1);
+                AmazonS3Client client = new AmazonS3Client(AccessKey, SecretKey, Amazon.RegionEndpoint.GetBySystemName(ClientRegion(RegionOfTheBucket)));
                 GetPreSignedUrlRequest Request = new GetPreSignedUrlRequest
                 {
                     BucketName = NameOfTheBucket,
