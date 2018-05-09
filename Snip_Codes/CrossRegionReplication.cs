d3cb6972024fbf1fef8037cadf162facfb2977ab
nautiyaldeepak <deepaknautiyal07@outlook.com>
2018-05-09 19:06:23 +0530

    Cross Region Replication

--- Snip_Codes/CrossRegionReplication.cs
+++ Snip_Codes/CrossRegionReplication.cs
@@ -8,6 +8,50 @@ namespace CrossRegionReplication
 {
     class Program
     {
+        public static string ClientRegion(string RegionOfClient)
+        {
+            switch (RegionOfClient)
+            {
+                case "ireland":
+                    return "eu-west-1";
+                case "mumbai":
+                    return "ap-south-1";
+                case "frankfurt":
+                    return "eu-central-1";
+                case "london":
+                    return "eu-west-2";
+                case "sydney":
+                    return "ap-southeast-2";
+                case "ohio":
+                    return "us-east-2";
+                case "virginia":
+                    return "us-east-1";
+                case "california":
+                    return "us-west-1";
+                case "oregon":
+                    return "us-west-2";
+                case "singapore":
+                    return "ap-southeast-1";
+                case "tokyo":
+                    return "ap-northeast-1";
+                case "canada":
+                    return "ca-central-1";
+                case "seoul":
+                    return "ap-northeast-2";
+                case "paris":
+                    return "eu-west-3";
+                case "sao paulo":
+                    return "sa-east-1";
+                case "osaka-local":
+                    return "ap-northeast-3";
+                case "beijing":
+                    return "cn-north-1";
+                case "ningxia":
+                    return "cn-northwest-1";
+            }
+            return "";
+        }
+
         static void Main(string[] args)
         {
             string AccessKey = " *** Enter Access Key *** ";
@@ -16,9 +60,10 @@ namespace CrossRegionReplication
             string Destination_Bucket = " *** Destination Bucket Here *** ";
             string Source_ObjectKey = " *** Source Object Key *** ";
             string Destination_ObjectKey = " *** Destination Object Key *** ";
+            string RegionOfTheBucket = " *** Enter the region of the bucket *** "
             
             //  Region for this client is mumbai -> APSouth1
-            AmazonS3Client client = new AmazonS3Client(AccessKey, SecretKey, Amazon.RegionEndpoint.APSouth1);
+            AmazonS3Client client = new AmazonS3Client(AccessKey, SecretKey, Amazon.RegionEndpoint.GetBySystemName(ClientRegion(RegionOfTheBucket)));
             try
             {
                 CopyObjectRequest Request = new CopyObjectRequest
